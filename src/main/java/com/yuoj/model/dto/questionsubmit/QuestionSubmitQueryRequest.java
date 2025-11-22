98b3a530df977c1c589a431039ef6b5690e66a91
yangxiao <374959214@qq.com>
2025-11-22 21:27:59 +0800

    后端项目初始化：用户、题目、题目提交等相关功能实现

---
+++ src/main/java/com/yuoj/model/dto/questionsubmit/QuestionSubmitQueryRequest.java
@@ -0,0 +1,68 @@
+package com.yuoj.model.dto.questionsubmit;
+
+
+import com.yuoj.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 题目分页请求
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class QuestionSubmitQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 用户代码
+     */
+    private String code;
+
+    /**
+     * 判题信息（json 对象）
+     */
+    private JudgeInfo judgeInfo;
+
+    /**
+     * 判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）
+     */
+    private Integer status;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+
+    private static final long serialVersionUID = 1L;
+}
